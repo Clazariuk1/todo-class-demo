731e73cbc763c6e02e8943535dfdb6a066325fd5
Clazariuk1 <Clazariuk@gmail.com>
2024-01-20 14:19:21 -0800

    completed api with unit testing

--- tests/todo.test.js
+++ tests/todo.test.js
@@ -44,5 +44,46 @@ describe('Testing Todo Endpoints for RESTFUL JSON API', () => {
 
         }
     })
+    test('It should create a new todo', async () => {
+        const response = await request(app).post('/todos').send({
+            title: 'my todo',
+            description: 'things I need to do',
+            completed: false
+        })
 
+        expect(response.body.title).toEqual('my todo')
+        expect(response.body.description).toEqual('things I need to do')
+        expect(response.body.completed).toBeFalsy()
+    })
+    test('given a valid body it should update an existing todo and return it', async () => {
+        const todo = new Todo({ title: 'test todo', description: 'test', completed: true })
+        await todo.save()
+
+        const response = await request(app).put(`/todos/${todo._id}`).send({
+            description: 'this is sparta'
+        })
+
+        expect(response.statusCode).toBe(200)
+        expect(response.body.description).toEqual('this is sparta')
+    })
+    test('It should delete a pre-existing todo given a valid todo id', async () => {
+        const todo = new Todo({ title: 'test todo', description: 'test', completed: true })
+        await todo.save()
+
+        const response = await request(app).delete(`/todos/${todo._id}`)
+
+        expect(response.statusCode).toBe(200)
+        expect(response.body.msg).toEqual(`The todo with the Id of ${todo._id}  was deleted from the MongoDB database, no further action necessary`)
+    })
+    test('It should show a pre-existing todo given a valid todo id', async () => {
+        const todo = new Todo({ title: 'test todo', description: 'test', completed: true })
+        await todo.save()
+
+        const response = await request(app).get(`/todos/${todo._id}`)
+
+        expect(response.statusCode).toBe(200)
+        expect(response.body.title).toEqual('test todo')
+        expect(response.body.description).toEqual('test')
+        expect(response.body.completed).toBeTruthy()
+    })
 })
